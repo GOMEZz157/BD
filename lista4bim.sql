a2c16cadb347df7d0b0c0adbbc3d2211783b006a
GOMEZz157 <nicksss.gomes@gmail.com>
2023-10-08 22:40:11 -0300

    exercicio_2_b

--- lista4bim.sql
+++ lista4bim.sql
@@ -24,3 +24,10 @@ CREATE TABLE produtos (
     preco NUMERIC(10, 2),
     quantidade INT
 );
+
+INSERT INTO produtos (produto, preco, quantidade) VALUES
+('Produto A', 19.99, 5),
+('Produto B', 29.99, -3),
+('Produto C', 9.99, 8);
+
+SELECT produto, ROUND(preco, 2) AS preco_arredondado FROM produtos;
